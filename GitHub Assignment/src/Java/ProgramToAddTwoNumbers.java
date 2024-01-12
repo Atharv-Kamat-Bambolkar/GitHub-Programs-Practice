550dc82138dad0b458fc0665797d41bf712ed39f
Atharv Kamat Bambolkar <rbca22114.rcca@gmail.com>
2024-01-12 16:22:31 +0530

    commit

--- GitHub Assignment/src/Java/ProgramToAddTwoNumbers.java
+++ GitHub Assignment/src/Java/ProgramToAddTwoNumbers.java
@@ -7,13 +7,21 @@ public class ProgramToAddTwoNumbers {
 	public static void main(String[] args) {
 		try (// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in)) {
-			int s, a, b;
+			int s1, s2, a, b;
 			System.out.println("Enter the two numbers: ");
 			a = sc.nextInt();
 			b = sc.nextInt();
-			s = a + b;
-			System.out.println("The sum of two numbers = " + s);
+			s1 = a + b;
+			System.out.println("The sum of two numbers = " + s1);
+			s2 = display(a, b);
+			System.out.println("The subtraction is " + s2);		
 		}
 	}
 
+	public static int display(int a, int b) {
+	    int s;
+		s = a - b;
+		return s;
+		
+	}
 }
