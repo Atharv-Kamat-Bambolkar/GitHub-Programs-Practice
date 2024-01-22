31ff29bc32ec2099b87ec935f7ebba4dac0aa519
Atharv Kamat Bambolkar <atharvkamatbambolkar2@gmail.com>
2024-01-22 12:27:41 +0530

    commit

---
+++ GitHub Assignment/src/githubPrograms/HelloWorld1.java
@@ -0,0 +1,13 @@
+package githubPrograms;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+	void display() {
+		System.out.println("To push this program in Ver2 to GitHub");
+	}
+}
